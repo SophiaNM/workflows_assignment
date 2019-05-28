a92a66c554eea44470e5b3d8e9706ee2e0c80628
SophiaNM <njerimurage92@outlook.com>
2019-05-28 21:07:30 +0200

    adding execute function for the process chaining Workflow

---
+++ assign2_workflowmodelling/execute.py
@@ -0,0 +1,16 @@
+# WPS Execute Operation
+
+import requests, os
+
+# loading payload of chaining process
+payload = open(os.path.dirname(os.path.abspath(__file__)) +"\\process_chaining.xml").read()
+
+# wpsServerUrl connection to WPS services
+wpsServerUrl = "http://130.89.221.193:85/geoserver/ows?"
+
+response = requests.post(wpsServerUrl, data=payload)
+print("Content-type: application/json")
+print()
+print(response.text)
+
+

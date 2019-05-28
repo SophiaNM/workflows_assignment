1ab2926bb7e4293a769bb55e980f1dd44869f7ae
SophiaNM <njerimurage92@outlook.com>
2019-05-28 20:57:12 +0200

    adding the correct wpserverurl and payload xml file

--- assign1_wps/execute.py
+++ assign1_wps/execute.py
@@ -2,9 +2,9 @@
 
 import requests, os
 
-payload = open(os.path.dirname(os.path.abspath(__file__)) +"\\envelope_geojson.xml").read()
+payload = open(os.path.dirname(os.path.abspath(__file__)) +"\\envelope_coord.xml").read()
 
-wpsServerUrl = "https://gisedu.itc.utwente.nl/student/s6040241/gpw/assign/wps.py?"
+wpsServerUrl = "https://gisedu.itc.utwente.nl/student/s6040241/gpw/assignment/assign1_wps/wps.py?"
 
 response = requests.post(wpsServerUrl, data=payload)
 print("Content-type: application/json")
